9a57e89c1f628a1c5aee327d90612e10c18679a2
imbrsk <boris696boris@gmail.com>
2025-10-21 17:21:47 +0200

    Refactor user management: update models, repositories, and services; remove obsolete files

--- iknow-api/Program.cs
+++ iknow-api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using iknow_api.Core.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<iknow_api.Repository.Interfaces.IUserRepository, iknow_api.Repository.UserRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,8 +23,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
     
 app.UseHttpsRedirection();
