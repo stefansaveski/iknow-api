615f18db50c22de8bb4d828806226a241ce22efe
Stefan-Saveski <stefansaveski19@gmail.com>
2025-12-10 13:44:07 +0100

    Add getSemesters endpoint and related data access
    Introduce a new `getSemesters` endpoint in `UserController.cs` to retrieve user semester data using JWT authentication.
    
    Add `DbSet<EnrolledSemesters>` to `AppDbContext.cs` for database interaction.
    
    Implement `GetUserSemestersAsync` in `UserRepository.cs` to fetch semesters by user ID, and update `IUserRepository.cs` to declare this method.
    
    Add `GetUserSemesters` method in `UserService.cs` to extract user ID from JWT and retrieve semesters, updating `IUserService.cs` accordingly.

--- iknow-api/Repository/Interface/IUserRepository.cs
+++ iknow-api/Repository/Interface/IUserRepository.cs
@@ -11,6 +11,7 @@ namespace iknow_api.Repositories
         Task AddHighSchoolAsync(HighSchool highSchool);
         Task<int> GetUsersCountAsync();
         Task<User?> GetUserByIdAsync(int id);
+        Task<List<EnrolledSemesters>?> GetUserSemestersAsync(int id);
         Task<User?> GetOnlyUserByIdAsync(int id);
 
     }
